f6a499e684aa9d6102973ed1752a371c57ed6635
Sagar Sahasrabuddhe <sagarsahasra@gmail.com>
2024-10-06 11:54:02 +0530

    Initial Commit

---
+++ Prime News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prime News
+//
+//  Created by Sagar Sahasrabuddhe on 06/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prime News/Prime_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prime_NewsApp.swift
+//  Prime News
+//
+//  Created by Sagar Sahasrabuddhe on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Prime_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
